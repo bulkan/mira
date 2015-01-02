e99cebacf31ce64d97e0d891c421b8e0d6b34afa
Bulkan Evcimen <bulkan@gmail.com>
2015-01-03 03:29:17 +1100

    plugin in dat.gui
    * moved mira fractal related logic into its own `class`

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 var PIXI = require('pixi.js');
 var Color = require('color');
 
+var DAT = require('dat-gui');
+
+var Mira = require('./lib/mira');
+
 var width = window.innerWidth ;
 var height = window.innerHeight;
 
-//var renderer = new PIXI.autoDetectRenderer(width, height);
+
 var renderer = new PIXI.CanvasRenderer(width, height);
 
 document.body.appendChild(renderer.view);
@@ -14,19 +18,12 @@ document.body.appendChild(renderer.view);
 var stage = new PIXI.Stage();
 var graphics = new PIXI.Graphics();
 
-var b = 0.9998;
-var a = 0.16;
-var c = 2 - 2 * a;
-var x = 0;
-var j = 0;
-var y =  6;
-var gen = 0;
-
-var scale = 20;
 
 var textFont = { font: "10px Arial", fill: "white"};
 
-var conditionsText = new PIXI.Text("a: " + a + " b: " + b, textFont);
+var mira = new Mira(width, height);
+var conditionsText = new PIXI.Text("a: " + mira.a + " b: " + mira.b, textFont);
+
 conditionsText.position.x = 20;
 conditionsText.position.y = 30;
 
@@ -37,19 +34,14 @@ var debugText = new PIXI.Text("", textFont);
 debugText.position.x = 20;
 debugText.position.y = 20;
 
-
 stage.addChild(debugText);
 
 var cl = Color("#FFCC00");
 
-
 graphics.beginFill(parseInt(cl.hexString().replace(/^#/,''), 16));
-
 graphics.blendMode = PIXI.blendModes.ADD;
 
 stage.addChild(graphics);
-
-
 
 var midX =  width / 2;
 var midY = height / 2;
@@ -63,30 +55,24 @@ var maxDistanceToCenter = lineDistance({x: 0, y: 0}, {x: midX, y: midY});
 
 var circle;
 
+var gui = new DAT.GUI();
+gui.add(mira, 'b', 0, 1).step(0.0001);
 
 function draw(){
-  gen++;
+  conditionsText.setText("a: " + mira.a + " b: " + mira.b, textFont);
 
-  debugText.setText("iteration: " + gen);
+  debugText.setText("iteration: " + mira.gen);
 
-  var z = x;
-  x = b * y + j;
+  var point = mira.nextIteration();
 
-  j = a * x + c * Math.pow(x, 2) / (1+ Math.pow(x, 2));
-  y = j - z;
+  var ratio = (lineDistance(point, midPoint) / maxDistanceToCenter);
 
-  var xp = (x * scale) + width / 2;
-  var yp= (y * scale) + height / 2;
+  circle = graphics.drawCircle(point.x, point.y, 2);
 
-  var ratio = (lineDistance({x: xp, y: yp}, midPoint) / maxDistanceToCenter);
-
-  circle = graphics.drawCircle(xp, yp, 2);
-
-  if(gen % 500 === 0){
+  if(mira.gen % 500 === 0){
     cl = cl.saturate(ratio * 1).mix(Color('green'), 0.1);
     graphics.beginFill(parseInt(cl.hexString().replace(/^#/,''), 16));
   }
-
 
   renderer.render(stage);
   requestAnimationFrame(draw);
@@ -106,4 +92,4 @@ function lineDistance(point1, point2){
 }
 
 
-requestAnimationFrame(draw);
+//requestAnimationFrame(draw);
