c075a8f3728772842d907c7a4c767dc92e487a52
Bulkan Evcimen <bulkan@gmail.com>
2020-02-10 22:22:10 +1100

    Use noise around point size

--- p5.js
+++ p5.js
@@ -9,68 +9,55 @@ const initializer = (sketch) => {
 	const mira = new Mira(45);
 	const palette = palettes[randomInt(0, palettes.length - 1)];
 
-	let currentColor = 0;
-
-	let div;
-
+	let colorIndex = 0;
 	let xoff = 0.0;
 	let yoff = 0.0;
 
 
-	let vertexCount = 0;
-	const MAX_VERTEX = 3;
-	
   sketch.setup = () => {
-		div =	sketch.createDiv();
     sketch.createCanvas(width, height);
 		sketch.background('black');
 		// sketch.blendMode(sketch.DIFFERENCE);
 
-		// sketch.noStroke();
 		sketch.colorMode(sketch.HSB, 255);
-
+		sketch.textSize(10);
   }
 	
   sketch.draw = () => {
 		const { x, y } = mira.nextIteration();
 
-		// if (vertexCount === 0) {
-		// 	sketch.noFill();
-		// 	sketch.beginShape(sketch.LINES);
-		// }
-		
-		// xoff += 0.1;
-		// yoff += 0.1;
+		sketch.noStroke();
+		sketch.fill('black');
+		sketch.rect(10, 30, 100, 100);
 
-		// let xNoiseVal = sketch.noise(xoff);
-		// let yNoiseVal = sketch.noise(yoff);
+		sketch.fill('white');
+		sketch.text( `${mira.iteration} of ${mira.maxIteration}`, 10, 30, 100, 100);
+		
+		xoff += 0.01;
+		yoff += 0.1;
+
+		let xNoiseVal = sketch.noise(xoff);
+		let yNoiseVal = sketch.noise(yoff);
 		
 		sketch.translate(width/2, height/2);
-		sketch.stroke(palette[currentColor]);
-		sketch.fill(palette[currentColor]);
+		sketch.stroke(palette[colorIndex]);
+		// sketch.fill(palette[colorIndex]);
 
-		// console.log(`vertextCount`, vertexCount);
-
-		// if (vertexCount <= MAX_VERTEX) {
-		// 	sketch.vertex(x , y);
-		// } 
-
-		// if (vertexCount > MAX_VERTEX) {
-		// 	console.log('endShape');
-		// 	vertexCount = 0;
-		// 	sketch.endShape();
-		// }
-
-		sketch.point(x , y , 2, 2);
-		vertexCount = vertexCount + 1;
+		sketch.strokeWeight(xNoiseVal * 10);
+		sketch.alpha(10 * yNoiseVal);
+		sketch.point(x, y, 2);
 
 		if (mira.maxIterationReached() ) {
 			sketch.noLoop();
 		}
-		currentColor = ( currentColor + palette.length - 1 ) % palette.length;
 
-		div.elt.innerText = `${mira.iteration} of ${mira.maxIteration}  -    ${mira.a} \t b: ${mira.b}`;
-  }
+		colorIndex = ( colorIndex + palette.length - 1 ) % palette.length;
+	}
+	
+	sketch.windowResized = () => {
+		// const { windowWidth, windowHeight } = sketch;
+		// sketch.resizeCanvas(windowWidth, windowHeight);
+	}
 }
 
 const P5 = new p5(initializer);
