d8ba63a2b4f55c0dfe2d5e7167633ed368bae6ac
Bulkan Evcimen <bulkan@gmail.com>
2020-02-11 22:02:45 +1100

    Refactor fractal function code
    * Use better variable names

--- p5.js
+++ p5.js
@@ -1,51 +1,68 @@
 const p5 = require('p5');
+const palettes = require('nice-color-palettes/1000');
 const Mira = require('./lib/mira');
-const palettes = require('nice-color-palettes');
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 const initializer = (sketch) => {
-	const mira = new Mira(45);
-	const palette = palettes[randomInt(0, palettes.length - 1)];
+	const mira = new Mira(25);
+	mira.maxIteration = 30000;
+
+	
+	mira.a = 0.4;
+	mira.b = 1;
+	mira.x = 12;
+	mira.y = 0;
+
+	// const palette = palettes[randomInt(0, palettes.length - 1)];
+	const palette = palettes[7];
 
 	let colorIndex = 0;
 	let xoff = 0.0;
 	let yoff = 0.0;
 
-
   sketch.setup = () => {
-    sketch.createCanvas(width, height);
+		
+		sketch.createCanvas(width, height);
 		sketch.background('black');
-		// sketch.blendMode(sketch.DIFFERENCE);
-
+		// sketch.blendMode(sketch.REMOVE);
+		
 		sketch.colorMode(sketch.HSB, 255);
 		sketch.textSize(10);
+
+		button = sketch.createButton('save');
+		button.position(10, 10);
+		button.mousePressed(() => sketch.saveCanvas(`${mira.a}-${mira.b}-(${mira.x}-${mira.y})`, 'png'));
   }
 	
   sketch.draw = () => {
 		const { x, y } = mira.nextIteration();
 
 		sketch.noStroke();
+		const black = sketch.color('black');
+		black.setAlpha(255);
 		sketch.fill('black');
 		sketch.rect(10, 30, 100, 100);
 
 		sketch.fill('white');
 		sketch.text( `${mira.iteration} of ${mira.maxIteration}`, 10, 30, 100, 100);
 		
-		xoff += 0.01;
+		xoff += 0.1;
 		yoff += 0.1;
 
 		let xNoiseVal = sketch.noise(xoff);
 		let yNoiseVal = sketch.noise(yoff);
 		
-		sketch.translate(width/2, height/2);
-		sketch.stroke(palette[colorIndex]);
-		// sketch.fill(palette[colorIndex]);
+		const color = sketch.color(palette[colorIndex]);
+		color.setAlpha(255 * yNoiseVal);
 
-		sketch.strokeWeight(xNoiseVal * 10);
-		sketch.alpha(10 * yNoiseVal);
-		sketch.point(x, y, 2);
+		sketch.translate(width / 2, height / 2);
+		sketch.stroke(color);
+
+		sketch.strokeWeight(sketch.map(xNoiseVal, 0, 1, 1, 3 ));
+		// sketch.point(sketch.noise(x) * x, y);
+		sketch.point(x, y);
 
 		if (mira.maxIterationReached() ) {
 			sketch.noLoop();
@@ -53,12 +70,9 @@ const initializer = (sketch) => {
 
 		colorIndex = ( colorIndex + palette.length - 1 ) % palette.length;
 	}
-	
-	sketch.windowResized = () => {
-		// const { windowWidth, windowHeight } = sketch;
-		// sketch.resizeCanvas(windowWidth, windowHeight);
-	}
 }
+
+
 
 const P5 = new p5(initializer);
 
